944106c6a0df4f7316df34c451aa8a285947beea
Anil Madhavapeddy <anil@recoil.org>
2016-12-16 16:10:51 +0000

    take 3 at travis

--- .travis.sh
+++ .travis.sh
@@ -11,7 +11,8 @@ docker run -w /mnt -v `pwd`:/mnt ocamllabs/furore pandoc -f markdown intro.md -o
 
 cd output
 git init
+git checkout -b gh-pages
 git add *
 git commit -m sync -a
 git remote add origin git@github.com:ocamllabs/furore
-git push origin +gh-pages
+git push -u origin +gh-pages
