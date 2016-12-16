4922a8689714eaaafca5b8dc5ed86577cc144354
Anil Madhavapeddy <anil@recoil.org>
2016-12-16 15:48:12 +0000

    add travis scripts

---
+++ .travis.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -ex
+
+eval `ssh-agent -s`
+chmod 600 furore_id_rsa
+ssh-add furore_id_rsa
+
+docker pull ocamllabs/furore
+rm -rf output
+mkdir output
+docker run -v `pwd`:/mnt pandoc -f markdown intro.md -o output/report.pdf
+
+cd output
+git init
+git add *
+git commit -m sync -a
+git remote add git@github.com:ocamllabs/furore
+git push origin +gh-pages
