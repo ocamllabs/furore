114455773f0b74cdf43edae1afba8cc1dcd253ab
Anil Madhavapeddy <anil@recoil.org>
2016-12-19 15:47:48 +0000

    add 2013 review from the archives

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,5 +3,10 @@
 docker build -t furore-local .
 # docker run -w /mnt -v `pwd`:/mnt furore-local pandoc --toc -f markdown `cat CHAPTERS.txt` -o output/report.pdf
 docker run -w /mnt -v `pwd`:/mnt furore-local pandoc --toc -f markdown `cat CHAPTERS.txt` -t html5 --css github-pandoc.css -o output/index.html
-docker run -w /mnt -v `pwd`:/mnt furore-local pandoc --toc -f markdown yearly/2015.md -t html5 --css github-pandoc.css -o output/2015.html
-docker run -w /mnt -v `pwd`:/mnt furore-local pandoc --toc -f markdown yearly/2015.md -t epub -o output/2015.epub
+
+YEARS="2014 2013"
+
+for y in ${YEARS}; do
+  docker run -w /mnt -v `pwd`:/mnt furore-local pandoc --toc -f markdown yearly/$y.md -t html5 --css github-pandoc.css -o output/$y.html
+  docker run -w /mnt -v `pwd`:/mnt furore-local pandoc --toc -f markdown yearly/$y.md -t epub -o output/$y.epub
+done
