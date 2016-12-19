69f441a00f4f94c1388b4919982235749b22825d
Anil Madhavapeddy <anil@recoil.org>
2016-12-19 14:09:55 +0000

    move to a local build that doesnt need Hub

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
 
-docker pull ocamllabs/furore
-# docker run -w /mnt -v `pwd`:/mnt ocamllabs/furore pandoc --toc -f markdown `cat CHAPTERS.txt` -o output/report.pdf
-docker run -w /mnt -v `pwd`:/mnt ocamllabs/furore pandoc --toc -f markdown `cat CHAPTERS.txt` -t html5 --css github-pandoc.css -o output/index.html
+docker build -t furore-local .
+# docker run -w /mnt -v `pwd`:/mnt furore-local pandoc --toc -f markdown `cat CHAPTERS.txt` -o output/report.pdf
+docker run -w /mnt -v `pwd`:/mnt furore-local pandoc --toc -f markdown `cat CHAPTERS.txt` -t html5 --css github-pandoc.css -o output/index.html
