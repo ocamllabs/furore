bfe77ba3c1edc92a0bfe0718d32f100ca7d2c8ae
Anil Madhavapeddy <anil@recoil.org>
2016-12-19 15:23:09 +0000

    deploy graphics correctly

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,8 @@ ssh-add furore_id_rsa
 cd output
 git init
 git checkout -b gh-pages
+rm graphics
+cp -r ../graphics .
 git add *
 git commit -m sync -a
 git remote add origin git@github.com:ocamllabs/furore
