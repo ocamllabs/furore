9950dcad2ac426b00bccf11ef4381206eef1ef9f
Anil Madhavapeddy <anil@recoil.org>
2016-12-19 10:19:22 +0000

    fix travis

--- .travis.sh
+++ .travis.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
 
 docker pull ocamllabs/furore
-docker run -w /mnt -v `pwd`:/mnt ocamllabs/furore pandoc --toc -f markdown intro.md -o output/report.pdf
-docker run -w /mnt -v `pwd`:/mnt ocamllabs/furore pandoc --toc -f markdown intro.md -t html5 --css github-pandoc.css -o output/index.html
+docker run -w /mnt -v `pwd`:/mnt ocamllabs/furore pandoc --toc -f markdown 01-intro.md -o output/report.pdf
+docker run -w /mnt -v `pwd`:/mnt ocamllabs/furore pandoc --toc -f markdown 01-intro.md -t html5 --css github-pandoc.css -o output/index.html
