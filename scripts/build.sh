900d6baaf2934586fa6929d6edbd6716d81ef341
Anil Madhavapeddy <anil@recoil.org>
2016-12-19 16:01:49 +0000

    finish porting 2014/2013 into one file

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,9 @@ docker build -t furore-local .
 # docker run -w /mnt -v `pwd`:/mnt furore-local pandoc --toc -f markdown `cat CHAPTERS.txt` -o output/report.pdf
 docker run -w /mnt -v `pwd`:/mnt furore-local pandoc --toc -f markdown `cat CHAPTERS.txt` -t html5 --css github-pandoc.css -o output/index.html
 
-YEARS="2014 2013"
+#for y in `cat YEARS.txt`; do
+#  docker run -w /mnt -v `pwd`:/mnt furore-local pandoc --toc -f markdown yearly/$y.md -t html5 --css github-pandoc.css -o output/$y.html
+#  docker run -w /mnt -v `pwd`:/mnt furore-local pandoc --toc -f markdown yearly/$y.md -t epub -o output/$y.epub
+#done
 
-for y in ${YEARS}; do
-  docker run -w /mnt -v `pwd`:/mnt furore-local pandoc --toc -f markdown yearly/$y.md -t html5 --css github-pandoc.css -o output/$y.html
-  docker run -w /mnt -v `pwd`:/mnt furore-local pandoc --toc -f markdown yearly/$y.md -t epub -o output/$y.epub
-done
+docker run -w /mnt -v `pwd`:/mnt furore-local pandoc --toc -f markdown yearly/intro.md yearly/2014.md yearly/2013.md --css github-pandoc.css -o output/yearly.html
---
+++ yearly/intro.md
@@ -0,0 +1,9 @@
+---
+title: OCaml Labs Annual Reviews
+author: Anil Madhavapeddy, Gemma Gordon, KC Sivaramakrishnan
+rights:  Creative Commons Non-Commercial Share Alike 3.0
+language: en-US
+---
+
+These are annual reviews of the activities of OCaml Labs -- an initiative within the Cambridge Computer Laboratory started by Anil Madhavapeddy in 2011 to promote research, growth and collaboration within the wider OCaml community. We contribute to management of the day-to-day OCaml maintenance load, and align research agendas with real-world projects to help progress the language and make it available and applicable to a larger audience. Together with INRIA and our other partners, our goal is to freely release and integrate all work upstream, allowing all prospective users access to the efficient, expressive language of OCaml.
+
