0fa97fba78c03f800d283c2bd9a6a2635313525d
Anil Madhavapeddy <anil@recoil.org>
2016-12-22 19:55:14 +0000

    fix build

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ RUN="docker run -w /mnt -v `pwd`:/mnt furore-local"
 
 # docker run -w /mnt -v `pwd`:/mnt furore-local pandoc --toc -f markdown `cat CHAPTERS.txt` -o output/report.pdf
 ${RUN} sh -c "cd weekly && rm -f weekly.ml.exe && opam config exec -- ./weekly.ml > weekly.md"
-${RUN} pandoc --toc -f markdown weekly/intro.md /weekly/weekly.md -t html5 --css github-pandoc.css -o output/weekly.html
+${RUN} pandoc --toc -f markdown weekly/intro.md weekly/weekly.md -t html5 --css github-pandoc.css -o output/weekly.html
 ${RUN} pandoc --toc -f markdown `cat CHAPTERS.txt` -t html5 --css github-pandoc.css -o output/index.html
 ${RUN} pandoc --toc -f markdown -S yearly/intro.md `cat YEARS.txt` --css github-pandoc.css -o output/yearly.html
 ${RUN} pandoc --toc --epub-cover-image=graphics/origami-camel.png -f markdown yearly/intro.md `cat YEARS.txt` -t epub -o output/yearly.epub
